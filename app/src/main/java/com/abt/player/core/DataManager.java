e60f87b6f1ce30b1c71e1045382c11bac1c7e879
huangweiqi <askviky2010@gmail.com>
2018-08-25 01:50:09 +0800

    完善DataManager

--- app/src/main/java/com/abt/player/core/DataManager.java
+++ app/src/main/java/com/abt/player/core/DataManager.java
@@ -1,9 +1,36 @@
 package com.abt.player.core;
 
+import com.abt.player.core.dao.Music;
+import com.abt.player.core.db.DbHelper;
+
+import java.util.List;
+
 /**
- * @描述： @
+ * @描述： @DataManager
  * @作者： @黄卫旗
  * @创建时间： @24/08/2018
  */
-public class DataManager {
+public class DataManager implements DbHelper {
+
+    private DbHelper mDbHelper;
+
+    public DataManager(DbHelper dbHelper) {
+        mDbHelper = dbHelper;
+    }
+
+    @Override
+    public List<Music> addMusic(String data) {
+        return mDbHelper.addMusic(data);
+    }
+
+    @Override
+    public void clearMusic() {
+        mDbHelper.clearMusic();
+    }
+
+    @Override
+    public List<Music> loadAllMusic() {
+        return mDbHelper.loadAllMusic();
+    }
+
 }
